29e82688235df8fe7ac4fadd307295e49e97dc2e
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-05-27 13:08:24 +0100

    Update lempoi.py

--- lempoi.py
+++ lempoi.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import pandas as pd
+from numpy import *
 
 # Create an empty DataFrame
 df = pd.DataFrame(columns=['Event', 'Time'])
-
+t = array([str])*19
 # Add a new row to the DataFrame based on user input
 def add_event():
-    event = st.text_input("Event")
+    for n in range(19):
+        event[n] = st.text_input("Event")
     time = st.selectbox("Time", ['09:00', '12:30', '15:45'])  # Use a selectbox for time selection
 
     if st.button("Add"):
