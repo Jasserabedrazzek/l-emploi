e465603ad9989606d2b4de02b8f18fea4d3be63c
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-05-27 12:56:32 +0100

    Update lempoi.py

--- lempoi.py
+++ lempoi.py
@@ -2,10 +2,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+for i in range(5):
+   df = pd.DataFrame(
+      np.random.randn(10, 5),
 
-df = pd.DataFrame(
-   np.random.randn(10, 5),
-   for i in range(5):
       columns=('col %d')
 
 st.table(df)
