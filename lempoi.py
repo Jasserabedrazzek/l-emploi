fbc908ef8611c514c88d78d9cea0f78efda43bb1
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-02 14:31:19 +0100

    Update lempoi.py

--- lempoi.py
+++ lempoi.py
@@ -1,17 +1,18 @@
 import streamlit as st
 import pandas as pd
-import datetime
 
 # Sample event data
-
 events = [
-    {'Event':text_input("") , 'Date': '2023-06-10', 'Location': 'City A'},
-    {'Event': 'Event 2', 'Date': '2023-06-15', 'Location': 'City B'},
-    {'Event': 'Event 3', 'Date': '2023-06-20', 'Location': 'City C'}
+    {'Event': 'Event 1', 'Date': '2023-06-10', 'Location': 'City A', 'Status': ''},
+    {'Event': 'Event 2', 'Date': '2023-06-15', 'Location': 'City B', 'Status': ''},
+    {'Event': 'Event 3', 'Date': '2023-06-20', 'Location': 'City C', 'Status': ''}
 ]
 
 # Convert event data to a Pandas DataFrame
 df = pd.DataFrame(events)
+
+# Add text input column
+df['Status'] = df['Status'].apply(st.text_input, args=('Enter status',))
 
 # Display the table using Streamlit
 st.table(df)
