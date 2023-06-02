bd324d84bfaa6720fba094faffa4613fc61db665
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-02 14:15:52 +0100

    Update lempoi.py

--- lempoi.py
+++ lempoi.py
@@ -3,7 +3,9 @@ import pandas as pd
 from numpy import *
 
 # Create an empty DataFrame
-df = pd.DataFrame(columns=['Event', 'Time'])
+df = pd.DataFrame(columns=['Event', 'Time'],for i in range(5))
 
 
+
+st.table(df)
 # Add a new row to the DataFrame based on user input
