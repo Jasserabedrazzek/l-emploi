b794da399b3e6e518a37bd6f49b76c437679005d
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-02 14:36:55 +0100

    Update lempoi.py

--- lempoi.py
+++ lempoi.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Sample event data
 events = [
-    {'Event': 'Event 1', 'Date': '2023-06-10'},
+    {'Event': st.text_input(""), 'Date': '2023-06-10'},
     {'Event': 'Event 2', 'Date': '2023-06-15'},
     {'Event': 'Event 3', 'Date': '2023-06-20'}
 ]
