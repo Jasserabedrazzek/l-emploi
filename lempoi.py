27d99befed6ea48881073280294876c0c9975d30
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-02 14:34:33 +0100

    Update lempoi.py

--- lempoi.py
+++ lempoi.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 # Sample event data
 events = [
-    {'Event': 'Event 1', 'Date': '2023-06-10', 'Location': 'City A', 'Status': ''},
-    {'Event': 'Event 2', 'Date': '2023-06-15', 'Location': 'City B', 'Status': ''},
-    {'Event': 'Event 3', 'Date': '2023-06-20', 'Location': 'City C', 'Status': ''}
+    {'Event': 'Event 1', 'Date': '2023-06-10'},
+    {'Event': 'Event 2', 'Date': '2023-06-15'),
+    {'Event': 'Event 3', 'Date': '2023-06-20'}
 ]
 
 # Convert event data to a Pandas DataFrame
