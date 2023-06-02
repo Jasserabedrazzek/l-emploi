d75d102b00f608472365c068f167e1c4d305b478
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-02 14:53:37 +0100

    Update lempoi.py

--- lempoi.py
+++ lempoi.py
@@ -1,18 +1,26 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+from numpy import *
 import datetime
 
+T = array([str]*19)
+D = array([str]*19)
 # Sample event data
-events = []
-
-for i in range(1, 19):
-    event_name = st.text_input(f'Event {i}:')
-    event_date[i] = st.date_input("When's?", datetime.date(2019, 7, i))
-    events.append({'Event': event_name, 'Date': str(event_date[i])})
+for i in range(1,19):
+    T[i] = st.text_input(f'envent {i} :')
+    D[i] = st.date_input(
+        "When\'s ?",
+        datetime.date(2019, 7, i))
+    
+events = [
+    {'Event': st.text_input(""), 'Date': '2023-06-10'},
+    {'Event': 'Event 2', 'Date': '2023-06-15'},
+    {'Event': 'Event 3', 'Date': '2023-06-20'}
+]
 
 # Convert event data to a Pandas DataFrame
 df = pd.DataFrame(events)
+
 
 # Display the table using Streamlit
 st.table(df)
