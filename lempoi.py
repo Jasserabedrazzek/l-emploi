370835886a209dd62b626af981d78bafbae87962
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-02 14:52:24 +0100

    Update lempoi.py

--- lempoi.py
+++ lempoi.py
@@ -8,8 +8,8 @@ events = []
 
 for i in range(1, 19):
     event_name = st.text_input(f'Event {i}:')
-    event_date = st.date_input("When's?", datetime.date(2019, 7, 6))
-    events.append({'Event': event_name, 'Date': str(event_date)})
+    event_date[i] = st.date_input("When's?", datetime.date(2019, 7, 6))
+    events.append({'Event': event_name, 'Date': str(event_date[i])})
 
 # Convert event data to a Pandas DataFrame
 df = pd.DataFrame(events)
