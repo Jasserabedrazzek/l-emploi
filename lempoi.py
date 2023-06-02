ec9779ffbaf7a43c074aeb2972c49d82ff16d38f
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-02 14:30:10 +0100

    Update lempoi.py

--- lempoi.py
+++ lempoi.py
@@ -5,7 +5,7 @@ import datetime
 # Sample event data
 
 events = [
-    {'Event':ev1 = text_input("") , 'Date': '2023-06-10', 'Location': 'City A'},
+    {'Event':text_input("") , 'Date': '2023-06-10', 'Location': 'City A'},
     {'Event': 'Event 2', 'Date': '2023-06-15', 'Location': 'City B'},
     {'Event': 'Event 3', 'Date': '2023-06-20', 'Location': 'City C'}
 ]
