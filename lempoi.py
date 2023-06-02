f7e9afd16bc1e150777708216d5bc0d9583b98b2
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-02 14:35:13 +0100

    Update lempoi.py

--- lempoi.py
+++ lempoi.py
@@ -4,7 +4,7 @@ import pandas as pd
 # Sample event data
 events = [
     {'Event': 'Event 1', 'Date': '2023-06-10'},
-    {'Event': 'Event 2', 'Date': '2023-06-15'),
+    {'Event': 'Event 2', 'Date': '2023-06-15'},
     {'Event': 'Event 3', 'Date': '2023-06-20'}
 ]
 
