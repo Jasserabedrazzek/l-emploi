7d007508056ed1e69fde8506f035756b5091eedf
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-02 14:21:05 +0100

    Update lempoi.py

--- lempoi.py
+++ lempoi.py
@@ -1,8 +1,17 @@
 import streamlit as st
 import pandas as pd
 from numpy import *
-
-# Create an empty DataFrame
+n = 0
+# Create an empty DataFrame*
+ok = False
+while ok == False:
+  add = st.button("ADD")
+  if add:
+    n+=1
+  T = array([str]*n)
+  for i n range(n):
+    T[i] = st.text_input(f"envent {i}:")
+    
 df = pd.DataFrame(columns=['Event', 'Time'],for i in range(5))
 
 
