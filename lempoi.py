611d888bafea59d3f0eabe541de996b7fb0041f7
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-02 14:29:44 +0100

    Update lempoi.py

--- lempoi.py
+++ lempoi.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pandas as pd
+import datetime
 
 # Sample event data
+
 events = [
-    {'Event': 'Event 1', 'Date': '2023-06-10', 'Location': 'City A'},
+    {'Event':ev1 = text_input("") , 'Date': '2023-06-10', 'Location': 'City A'},
     {'Event': 'Event 2', 'Date': '2023-06-15', 'Location': 'City B'},
     {'Event': 'Event 3', 'Date': '2023-06-20', 'Location': 'City C'}
 ]
