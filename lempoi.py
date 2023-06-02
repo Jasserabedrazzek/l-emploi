d2ada9dc7c943530f0a59a0e03676802847659ff
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-06-02 14:57:48 +0100

    Update lempoi.py

--- lempoi.py
+++ lempoi.py
@@ -5,7 +5,7 @@ import datetime
 
 T = array([str]*19)
 D = array([str]*19)
-df = array([str]*19)
+
 # Sample event data
 for i in range(1,19):
     T[i] = st.text_input(f'envent {i} :')
@@ -19,7 +19,7 @@ for i in range(1,19):
     ]
 
 # Convert event data to a Pandas DataFrame
-    df[i] = pd.DataFrame(events)
+    df = pd.DataFrame(events)
 
 
 # Display the table using Streamlit
